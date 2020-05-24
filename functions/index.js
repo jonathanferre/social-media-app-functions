747cb70cc0a4f26d7239b240a8300cc5b7725783
Jonathan <jonathanferre2@gmail.com>
2020-05-24 03:07:22 -0300

    Create & Read Data

--- functions/index.js
+++ functions/index.js
@@ -26,3 +26,27 @@ exports.getScreams = functions.https.onRequest((req, res) => {
     })
     .catch((err) => console.error(err));
 });
+
+exports.createScream = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    return res.status(400).json({ error: "Method not allowed" });
+  }
+
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+
+  admin
+    .firestore()
+    .collection("scream")
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something wrong" });
+      console.error(err);
+    });
+});
