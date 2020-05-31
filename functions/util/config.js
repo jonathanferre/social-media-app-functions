aa256dad092569d1b174ca11c89198c351b8b7d4
Jonathan <jonathanferre2@gmail.com>
2020-05-31 20:11:56 -0300

    Refactoring & Organising

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyAPCB7z0FY3RE2jr2g0cQP_Mh84G0dxhwI",
+  authDomain: "social-media-app-d305a.firebaseapp.com",
+  databaseURL: "https://social-media-app-d305a.firebaseio.com",
+  projectId: "social-media-app-d305a",
+  storageBucket: "social-media-app-d305a.appspot.com",
+  messagingSenderId: "468674202258",
+  appId: "1:468674202258:web:f8b0c3bb1c5f2a5c8df44b",
+  measurementId: "G-KHKQ9D3MBV",
+};
