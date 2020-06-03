28d87308002b3f87e6c974b80bf928e04d1e6e8e
Jonathan <jonathanferre2@gmail.com>
2020-06-03 19:22:54 -0300

    Add and Get User profile details

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -5,8 +5,13 @@ const config = require("../util/config");
 const firebase = require("firebase");
 firebase.initializeApp(config);
 
-const { validateSignupData, validateLoginData } = require("../util/validators");
+const {
+  validateSignupData,
+  validateLoginData,
+  reduceUserDetails,
+} = require("../util/validators");
 
+// Sign users up
 exports.signup = (req, res) => {
   const newUser = {
     email: req.body.email,
@@ -62,6 +67,7 @@ exports.signup = (req, res) => {
     });
 };
 
+// Log user in
 exports.login = (req, res) => {
   const user = {
     email: req.body.email,
@@ -91,6 +97,50 @@ exports.login = (req, res) => {
     });
 };
 
+// Add user details
+exports.addUserDetails = (req, res) => {
+  let userDetails = reduceUserDetails(req.body);
+
+  db.doc(`/users/${req.user.handle}`)
+    .update(userDetails)
+    .then(() => {
+      return res.json({ message: "Details added successfully" });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
+// Get own user details
+exports.getAuthenticatedUser = (req, res) => {
+  let userData = {};
+
+  db.doc(`/users/${req.user.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        userData.credentials = doc.data();
+        return db
+          .collection("likes")
+          .where("userHandle", "==", req.user.handle)
+          .get();
+      }
+    })
+    .then((data) => {
+      userData.likes = [];
+      data.forEach((doc) => {
+        userData.likes.push(doc.data());
+      });
+      return res.json(userData);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
+//Upload a profile image for user
 exports.uploadImage = (req, res) => {
   const Busboy = require("busboy");
   const path = require("path");
@@ -103,15 +153,12 @@ exports.uploadImage = (req, res) => {
   let imageToBeUploaded = {};
 
   busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-    console.log("fieldname: ", fieldname);
-    console.log("filename: ", filename);
-    console.log("mimetype: ", mimetype);
-
+    console.log(fieldname, file, filename, encoding, mimetype);
     if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
       return res.status(400).json({ error: "Wrong file type submitted" });
     }
 
-    // my.image.png
+    // my.image.png => ['my', 'image', 'png']
     const imageExtension = filename.split(".").pop();
     // 987654312.png
     imageFileName = `${Math.round(
