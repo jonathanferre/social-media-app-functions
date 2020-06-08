ddae1c76890592d2148ecc2ee23a6745ba49de88
Jonathan <jonathanferre2@gmail.com>
2020-06-07 22:06:40 -0300

    Like, Unlike and Delete Scream

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,9 @@ const {
   postOneScream,
   getScream,
   commentOnScream,
+  likeScream,
+  unlikeScream,
+  deleteScream,
 } = require("./handlers/screams");
 const {
   signup,
@@ -20,10 +23,10 @@ const {
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
 app.get("/scream/:screamId", getScream);
-//TODO: delete scream
-//TODO: like a scream
-//TODO: unlike a scream
+app.get("/scream/:screamId/like", FBAuth, likeScream);
+app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
 app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
+app.delete("/scream/:screamId", FBAuth, deleteScream);
 
 // Users routes
 app.post("/signup", signup);
